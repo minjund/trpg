7710739545e1cdf84d13b21daf94e90359ce0558
minjund <mam060922@naver.com>
2022-06-07 01:02:59 +0900

    trpg 프로젝트 시작

---
+++ src/main/kotlin/com/trpg/trpg/TrpgApplication.kt
@@ -0,0 +1,11 @@
+package com.trpg.trpg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrpgApplication
+
+fun main(args: Array<String>) {
+    runApplication<TrpgApplication>(*args)
+}
